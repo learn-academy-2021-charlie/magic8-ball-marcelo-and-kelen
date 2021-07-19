1b36fe8833cacecbc915de8de402af474ae80087
celoazevedo <meazevedo86@gmail.com>
2021-07-19 15:58:16 -0700

    yoo let try this

--- src/App.js
+++ src/App.js
@@ -6,16 +6,41 @@ class App extends Component {
     super(props)
     this.state = {
       // ACTION ITEM: Add possible Magic8 Ball answers to the array
-      answerArray: ["Here is an answer"],
-      selectedAnswer: ""
+      answerArray: [
+        "Here is an answer", 
+        "It is Certain", 
+        "It is decidedly so", 
+        " Without a doubt"
+      ],
+      selectedAnswer: "",
+      input: ""
     }
+  }
+
+  handleChange = (e) => {
+    console.log(this.state)
+    this.setState({
+      input: e.target.value
+    })
+    console.log(e.target.value)
   }
 
   handleSubmit = () => {
     const { answerArray } = this.state
     // ACTION ITEM: Your code here!!
     // ACTION ITEM: Update the answerArray selection
-    this.setState({ selectedAnswer: answerArray[0] })
+    this.setState({ 
+      selectedAnswer: 
+        answerArray[Math.floor(Math.random()*4)] })
+    }
+
+
+  // trying to clear input box!!!!                             
+  handleClear = () => {                  
+    this.setState({
+      input: ''
+    }) 
+    console.log(this.state) 
   }
 
   render(){
@@ -25,12 +50,17 @@ class App extends Component {
         <input
           className="inputBox"
           type="text"
+          onChange={ this.handleChange }
+          value={ this.state.input }
         />
         <br />
         <button onClick={ this.handleSubmit }>
           Ask the Magic 8 Ball a Question
         </button>
         <p>{ this.state.selectedAnswer }</p>
+        <button onClick={ this.handleClear }>
+          clear
+        </button>
       </>
     )
   }
